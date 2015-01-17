039d3e948d855e1bcc00c77a5124ee17add6b199
Elliot Chance <elliotchance@gmail.com>
2015-01-17 19:29:53 +1100

    Company

--- kounta.py
+++ kounta.py
@@ -1,5 +1,16 @@
+from dateutil.parser import parse
+
 class Object:
+    """
+    Used as the parent for all objects returned from the API. It main purpose is
+    to allow documentation to be built into the object instead of using plain
+    `dict`s.
+    """
+
     def __init__(self, obj):
+        """
+        :type dict: object
+        """
         self.obj = obj
 
     def __getattr__(self, item):
@@ -72,3 +83,159 @@ class Company(Object):
         :return: integer
         """
         return self.obj['id']
+
+    @property
+    def name(self):
+        """
+        Company name.
+        :return: string
+        """
+        return self.obj['name']
+
+    @property
+    def shipping_address(self):
+        """
+        Shipping address.
+        :return: Address
+        """
+        shipping_address = self.obj['shipping_address']
+        return Address(shipping_address) if shipping_address else None
+
+    @property
+    def postal_address(self):
+        """
+        Postal address.
+        :return: Address
+        """
+        postal_address = self.obj['postal_address']
+        return Address(postal_address) if postal_address else None
+
+    @property
+    def addresses(self):
+        """
+        Total addresses available.
+        :return: object
+        """
+        addresses = self.obj['addresses']
+        addresses['updated_at'] = parse(addresses['updated_at'])
+        return addresses
+
+    @property
+    def business_number(self):
+        """
+        ABN, ACN or whatever is applicable as the business number.
+        :return: string
+        """
+        return self.obj['business_number']
+
+    @property
+    def contact_staff_member(self):
+        """
+        Contact staff member.
+        :return: Staff
+        """
+        return Staff(self.obj['contact_staff_member'])
+
+    @property
+    def image(self):
+        """
+        Avatar image.
+        :return: string
+        """
+        return self.obj['image']
+
+    @property
+    def website(self):
+        """
+        Website.
+        :return: string
+        """
+        return self.obj['website']
+
+    @property
+    def currency(self):
+        """
+        Currency code.
+        :return: string
+        """
+        return self.obj['currency']
+
+    @property
+    def timezone(self):
+        """
+        Timezone information.
+        :return: Timezone
+        """
+        return Timezone(self.obj['timezone'])
+
+    @property
+    def sites(self):
+        """
+        Sites.
+        :return: object
+        """
+        sites = self.obj['sites']
+        sites['updated_at'] = parse(sites['updated_at'])
+        return sites
+
+    @property
+    def registers(self):
+        """
+        Registers.
+        :return: object
+        """
+        registers = self.obj['registers']
+        registers['updated_at'] = parse(registers['updated_at'])
+        return registers
+
+    @property
+    def created_at(self):
+        """
+        When the company was created.
+        :return: datetime
+        """
+        return parse(self.obj['created_at'])
+
+    @property
+    def updated_at(self):
+        """
+        When the company was last modified.
+        :return: datetime
+        """
+        return parse(self.obj['updated_at'])
+
+class Permission(Object):
+    @property
+    def code(self):
+        """
+        :rtype : string
+        """
+        return self.obj['code']
+
+    @property
+    def name(self):
+        """
+        :rtype : string
+        """
+        return self.obj['name']
+
+    @property
+    def domain(self):
+        """
+        :rtype : string
+        """
+        return self.obj['domain']
+
+class Timezone(Object):
+    @property
+    def offset(self):
+        return self.obj['offset']
+
+    @property
+    def name(self):
+        return self.obj['name']
+
+class Staff(Object):
+    @property
+    def offset(self):
+        return self.obj['offset']
--- test/test_company.py
+++ test/test_company.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-from kounta import Company
+from kounta import Address, Company, Staff, Timezone
 import json
+from dateutil.parser import parse
 
 class TestCompany(TestCase):
     def setUp(self):
@@ -9,3 +10,63 @@ class TestCompany(TestCase):
 
     def test_id(self):
         self.assertEqual(self.company.id, 5678)
+
+    def test_name(self):
+        self.assertEqual(self.company.name, "Rockin' & Rollin' Again")
+
+    def test_shipping_address(self):
+        self.assertEqual(self.company.shipping_address, None)
+
+    def test_postal_address(self):
+        postal_address = self.company.postal_address
+        self.assertTrue(isinstance(postal_address, Address))
+        self.assertEqual(postal_address.id, 198109)
+
+    def test_addresses(self):
+        self.assertEqual(self.company.addresses['count'], 3)
+        self.assertEqual(self.company.addresses['updated_at'],
+                         parse('2013-05-22T16:21:40+10:00'))
+
+    def test_business_number(self):
+        self.assertEqual(self.company.business_number, "63 987 012 468")
+
+    def test_contact_staff_member(self):
+        contact_staff_member = self.company.contact_staff_member
+        self.assertTrue(isinstance(contact_staff_member, Staff))
+        self.assertEqual(contact_staff_member.id, 92402)
+
+    def test_image(self):
+        image = ("http://www.gravatar.com/avatar/"
+            "b8e42a8b1eb5967f80bf085adb97d613.jpg")
+        self.assertEqual(self.company.image, image)
+
+    def test_website(self):
+        self.assertEqual(self.company.website, "http://rara.kom")
+
+    def test_currency(self):
+        self.assertEqual(self.company.currency, "AUD")
+
+    def test_timezone(self):
+        timezone = self.company.timezone
+        self.assertTrue(isinstance(timezone, Timezone))
+        self.assertEqual(timezone.offset, '+10:00')
+        self.assertEqual(timezone.name, 'Australia/Melbourne')
+
+    def test_sites(self):
+        self.assertEqual(self.company.sites['count'], 3)
+        self.assertEqual(self.company.sites['updated_at'],
+                         parse('2013-05-24T16:24:13+10:00'))
+
+    def test_registers(self):
+        self.assertEqual(self.company.registers['count'], 12)
+        self.assertEqual(self.company.registers['limit'], 15)
+        self.assertEqual(self.company.registers['updated_at'],
+                         parse('2013-05-22T12:18:44+10:00'))
+
+    def test_created_at(self):
+        self.assertEqual(self.company.created_at,
+                         parse("2013-05-08T13:56:02+10:00"))
+
+    def test_updated_at(self):
+        self.assertEqual(self.company.updated_at,
+                         parse("2013-05-22T16:21:40+10:00"))
