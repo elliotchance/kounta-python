528a69f5865da63126bd2c8c52272e3ee55e96da
Elliot Chance <elliotchance@gmail.com>
2015-01-24 12:13:02 +1100

    test_unprocessed

--- kounta/cashup.py
+++ kounta/cashup.py
@@ -1,3 +1,5 @@
 class CashupUrlGenerator:
-    def get_url(self):
+    def get_url(self, **kwargs):
+        if 'unprocessed' in kwargs.keys():
+            return 'cashups/unprocessed.json'
         return 'cashups.json'
--- test/test_cashups.py
+++ test/test_cashups.py
@@ -5,3 +5,8 @@ class TestCashupUrlGenerator(TestCase):
     def test_no_filter(self):
         generator = CashupUrlGenerator()
         self.assertEqual(generator.get_url(), 'cashups.json')
+
+    def test_unprocessed(self):
+        generator = CashupUrlGenerator()
+        self.assertEqual(generator.get_url(unprocessed=True),
+                         'cashups/unprocessed.json')
