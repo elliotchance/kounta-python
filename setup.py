494df5d9d3a724fb5deb5ca554c640d33e9de816
Elliot Chance <elliotchance@gmail.com>
2015-08-12 09:49:59 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 setup(
     name='kounta',
     version='1.0',
-    py_modules=['kounta'],
+    packages = find_packages()
+    author='Elliot Chance',
+    author_email='elliotchance@gmail.com',
+    license='MIT',
+    keywords='kounta',
+    url='https://github.com/elliotchance/kounta-python',
+    install_requires = ['python-dateutil'],
 )
