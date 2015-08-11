ed732e14670cebe0a191f8077f5c1abb3ac6728f
Elliot Chance <elliotchance@gmail.com>
2015-08-12 09:50:20 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, find_packages
 setup(
     name='kounta',
     version='1.0',
-    packages = find_packages()
+    packages = find_packages(),
     author='Elliot Chance',
     author_email='elliotchance@gmail.com',
     license='MIT',
